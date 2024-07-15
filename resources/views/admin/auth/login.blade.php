b2c3cc5691d24ed70e915b793f06ffac2690aa57
nhathuy <lamnhathuy0393418721@gmail.com>
2024-07-15 13:00:18 +0000

    hotfix: chỉnh sửa giao diện đăng nhập, thêm trường select cơ sở và xóa trường password

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -6,15 +6,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Đăng nhập</title>
     <link rel="stylesheet" href="{{ asset('admin') }}/css/customize.css">
+
 </head>
 
 <body>
     <div class="login-container">
         <h2>Đăng Nhập</h2>
+        <button class="google-login">Đăng nhập với Google</button>
+        <div class="divider">hoặc đăng nhập với</div>
         <form action="#" method="POST">
             @csrf
             <input type="email" name="email" placeholder="Email" required>
-            <input type="password" name="password" placeholder="Mật Khẩu" required>
+            <select name="branch" class="branch">
+                <option value="">Cần Thơ</option>
+                <option value="">Hồ Chí Minh</option>
+            </select>
             <div class="options">
                 <a href="#">Quên mật khẩu?</a>
             </div>
