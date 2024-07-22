fdc409b41e3dba21b8f1ae8e11961997fe1efdf5
Nguyen-Quoc-Huy-div <huynqpc05332@fpt.edu.vn>
2024-07-22 13:13:38 +0700

    hotfix: giao diện quản lý sinh viên

--- resources/views/admin/student/student/pages/index.blade.php
+++ resources/views/admin/student/student/pages/index.blade.php
@@ -29,44 +29,7 @@
                                             aria-controls="basic-datatables"></label></div>
                             </div>
                         </div>
-                        @include('admin.student.student.components.table');
-                        <div class="row">
-                            <div class="col-sm-12 col-md-5">
-                                <div class="dataTables_info" id="basic-datatables_info" role="status"
-                                    aria-live="polite">Hiển thị 1 đến 10 của 57 bản ghi</div>
-                            </div>
-                            <div class="col-sm-12 col-md-7">
-                                <div class="dataTables_paginate paging_simple_numbers" id="basic-datatables_paginate">
-                                    <ul class="pagination">
-                                        <li class="paginate_button page-item previous disabled"
-                                            id="basic-datatables_previous"><a href="#"
-                                                aria-controls="basic-datatables" data-dt-idx="0" tabindex="0"
-                                                class="page-link">Trước</a></li>
-                                        <li class="paginate_button page-item active"><a href="#"
-                                                aria-controls="basic-datatables" data-dt-idx="1" tabindex="0"
-                                                class="page-link">1</a></li>
-                                        <li class="paginate_button page-item "><a href="#"
-                                                aria-controls="basic-datatables" data-dt-idx="2" tabindex="0"
-                                                class="page-link">2</a></li>
-                                        <li class="paginate_button page-item "><a href="#"
-                                                aria-controls="basic-datatables" data-dt-idx="3" tabindex="0"
-                                                class="page-link">3</a></li>
-                                        <li class="paginate_button page-item "><a href="#"
-                                                aria-controls="basic-datatables" data-dt-idx="4" tabindex="0"
-                                                class="page-link">4</a></li>
-                                        <li class="paginate_button page-item "><a href="#"
-                                                aria-controls="basic-datatables" data-dt-idx="5" tabindex="0"
-                                                class="page-link">5</a></li>
-                                        <li class="paginate_button page-item "><a href="#"
-                                                aria-controls="basic-datatables" data-dt-idx="6" tabindex="0"
-                                                class="page-link">6</a></li>
-                                        <li class="paginate_button page-item next" id="basic-datatables_next"><a
-                                                href="#" aria-controls="basic-datatables" data-dt-idx="7"
-                                                tabindex="0" class="page-link">Tiếp</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
+                        @include('admin.student.student.components.table')
                     </div>
                 </div>
             </div>
