8e45ee13d0e191e872349e3c0a5b58ab81e552d6
lphdev04 <lphdev04@gmail.com>
2024-07-27 12:39:04 +0700

    Database: thêm bảng phòng học

---
+++ database/seeders/ClassroomSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Classroom;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ClassroomSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Classroom::insert([
+            [
+                'name' => 'F202',
+                'description' => 'Phòng Học F202',
+                'created_by' => 1,
+                'created_at' => now(),
+                'updated_by' => null,
+                'updated_at' => null,
+                'deleted_by' => null,
+                'deleted_at' => null,
+            ],
+            [
+                'name' => 'F301',
+                'description' => 'Phòng Học F301',
+                'created_by' => 1,
+                'created_at' => now(),
+                'updated_by' => null,
+                'updated_at' => null,
+                'deleted_by' => null,
+                'deleted_at' => null,
+            ],
+        ]);
+    }
+}
