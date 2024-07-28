f1eaab0b8c1399ac5f470dcd440743a33bd96e94
nhathuy <lamnhathuy0393418721@gmail.com>
2024-07-28 10:50:36 +0700

    hotfix: sửa migrate major thêm course_id

--- resources/views/admin/major/major/components/table.blade.php
+++ resources/views/admin/major/major/components/table.blade.php
@@ -3,24 +3,17 @@
         <div class="row">
             <div class="col-sm-12 col-md-6">
                 <div class="dataTables_length" id="basic-datatables_length">
-                    <label>Hiển thị:
-                        <select name="basic-datatables_length" aria-controls="basic-datatables"
-                                class="form-control form-control-sm">
-                            <option value="10">10</option>
-                            <option value="25">25</option>
-                            <option value="50">50</option>
-                            <option value="100">100</option>
-                        </select>
-                        bản ghi
-                    </label>
+
                 </div>
             </div>
             <div class="col-sm-12 col-md-6">
                 <div id="basic-datatables_filter" class="dataTables_filter">
-                    <label>Tìm kiếm:
-                        <input type="search" class="form-control form-control-sm" placeholder=""
-                               aria-controls="basic-datatables">
-                    </label>
+                    <form action="{{ route('major.index') }}" method="GET">
+                        <label>Tìm kiếm:
+                            <input type="search" name="search" value="{{ request('search') }}" class="form-control form-control-sm" placeholder="Nhập tên hoặc mã ngành" aria-controls="basic-datatables">
+                        </label>
+                        <button type="submit" class="btn btn-primary btn-sm">Tìm kiếm</button>
+                    </form>
                 </div>
             </div>
         </div>
