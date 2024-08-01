a3b5dae101c91c2b86e4796c7ca10c8f0296dcdc
nhathuy <lamnhathuy0393418721@gmail.com>
2024-08-01 16:02:06 +0700

    hotfix: seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -29,7 +29,8 @@ class DatabaseSeeder extends Seeder
             ChatsSeeder::class,
             SubjectTypeSeeder::class,
             DepartmentSeeder::class,
-            StudyStatusSeeder::class
+            StudyStatusSeeder::class,
+            StudentsSeeder::class
         ]);
     }
 }
