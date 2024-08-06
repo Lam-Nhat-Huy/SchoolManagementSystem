6eb304b0096b78d9c0798c6723f213d80e572322
anhhoai <phamanhhoaipl@mail.com>
2024-08-06 23:58:37 +0700

    hotfix: Thêm tài liệu giảng viên (bên teacher)

--- app/Models/TeachingMaterial.php
+++ app/Models/TeachingMaterial.php
@@ -2,15 +2,31 @@
 
 namespace App\Models;
 
+use App\Models\TrainingOfficer\TrainingOfficerAccount;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class TeachingMaterial extends Model
 {
-    protected $fillable = ['teacher_id', 'title', 'description', 'file_path'];
+    use HasFactory;
 
-    public function teacher()
+    protected $fillable = [
+        'officer_id',
+        'course_id',
+        'title',
+        'description',
+        'file_path',
+    ];
+
+    public function officer()
     {
-        return $this->belongsTo(Teachers::class);
+        return $this->belongsTo(TrainingOfficerAccount::class, 'officer_id');
+    }
+
+    public function course()
+    {
+        return $this->belongsTo(Courses::class, 'course_id');
     }
 }
+
