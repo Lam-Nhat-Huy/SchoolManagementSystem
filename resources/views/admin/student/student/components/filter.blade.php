e470b7a8695873dcc78c7d1040b2170678267d4f
Nguyen-Quoc-Huy-div <huynqpc05332@fpt.edu.vn>
2024-08-06 21:55:18 +0700

    Hotfix: filter student

--- resources/views/admin/student/student/components/filter.blade.php
+++ resources/views/admin/student/student/components/filter.blade.php
@@ -1,6 +1,21 @@
+<div class="col-sm-12 col-md-5">
+    <div class="dataTables_length" id="basic-datatables_length"><label>Hiển thị:
+            <select name="sort" onchange="handleRedirect(this)"
+                aria-controls="basic-datatables" class="form-control form-control-sm">
+                <option value="{{ route('student.index') }}?sort=10"
+                    {{ request('sort') == 10 ? 'selected' : '' }}>10</option>
+                <option value="{{ route('student.index') }}?sort=25"
+                    {{ request('sort') == 25 ? 'selected' : '' }}>25</option>
+                <option value="{{ route('student.index') }}?sort=50"
+                    {{ request('sort') == 50 ? 'selected' : '' }}>50</option>
+                <option value="{{ route('student.index') }}?sort=100"
+                    {{ request('sort') == 100 ? 'selected' : '' }}>100</option>
+            </select>
+    </div>
+</div>
 <div class="col-sm-12 col-md-7">
     <div id="basic-datatables_filter" class="dataTables_filter ">
-        <form class="d-flex align-items-center justify-content-end" action="{{ route('student.index') }}">
+        <form class="d-flex align-items-center justify-content-end" action="{{ route('student.index') }}">       
             <div class="col-md-5">
                 <select class="form-select setupSelect2" name="major_id">
                     <option value="">Chọn chuyên ngành</option>
